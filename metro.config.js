ae79cd2266e20238e92cde3f1327fa3149227776
Ayush <>
2025-08-11 01:16:16 +0530

    Fix GitHub Pages deployment paths

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Configure for GitHub Pages deployment
+config.resolver.platforms = ['web', 'native', 'ios', 'android'];
+
 module.exports = config;
